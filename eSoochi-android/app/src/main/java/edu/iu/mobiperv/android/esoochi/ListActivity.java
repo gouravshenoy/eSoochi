3c2a11477f3458e558b42139a7441fb4aa2192d0
MANGIRISH WAGLE <vaglomangirish@gmail.com>
2016-09-18 17:11:58 -0400

    Adding the floating add button

--- eSoochi-android/app/src/main/java/edu/iu/mobiperv/android/esoochi/ListActivity.java
+++ eSoochi-android/app/src/main/java/edu/iu/mobiperv/android/esoochi/ListActivity.java
@@ -2,6 +2,7 @@ package edu.iu.mobiperv.android.esoochi;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,10 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.iu.mobiperv.android.esoochi.edu.iu.mobiperv.android.esoochi.util.Constants;
+import edu.iu.mobiperv.android.esoochi.edu.iu.mobiperv.android.esoochi.util.GlobalUtils;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -24,6 +29,7 @@ public class ListActivity extends AppCompatActivity {
     //private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,80 @@ public class ListActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d(TAG, "Tab Selected: " + tab.getText());
+
+                if(tab.getText().equals(Constants.ITEM_LIST_TAG)) {
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Log.d(TAG, "Clicked : " + Constants.ITEM_LIST_TAG);
+                            final EditText taskEditText = new EditText(fab.getContext());
+                            AlertDialog dialog = new AlertDialog.Builder(fab.getContext())
+                                    .setTitle("Add a new item")
+                                    .setMessage("Enter item name")
+                                    .setView(taskEditText)
+                                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            String item = String.valueOf(taskEditText.getText());
+                                            Log.d(TAG, "Item to add: " + item);
+
+                                            GlobalUtils.itemList.add(item);
+
+
+                                        }
+                                    })
+                                    .setNegativeButton("Cancel", null)
+                                    .create();
+                            dialog.show();
+                        }
+                    });
+                }
+                else if(tab.getText().equals(Constants.GROUP_LIST_TAG)) {
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Log.d(TAG, "Clicked : " + Constants.GROUP_LIST_TAG);
+                            final EditText taskEditText = new EditText(fab.getContext());
+                            AlertDialog dialog = new AlertDialog.Builder(fab.getContext())
+                                    .setTitle("Add a new group")
+                                    .setMessage("Enter group name")
+                                    .setView(taskEditText)
+                                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            String group = String.valueOf(taskEditText.getText());
+                                            Log.d(TAG, "Group to add: " + group);
+
+                                            GlobalUtils.groupList.add(group);
+
+                                        }
+                                    })
+                                    .setNegativeButton("Cancel", null)
+                                    .create();
+                            dialog.show();
+                        }
+                    });
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        fab = (FloatingActionButton) findViewById(R.id.add_list_item_button);
     }
 
     @Override
@@ -63,7 +143,6 @@ public class ListActivity extends AppCompatActivity {
                                 String task = String.valueOf(taskEditText.getText());
                                 Log.d(TAG, "Item to add: " + task);
 
-                                
                             }
                         })
                         .setNegativeButton("Cancel", null)
@@ -79,8 +158,8 @@ public class ListActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ItemFragment(), "My Items");
-        adapter.addFragment(new GroupFragment(), "Groups");
+        adapter.addFragment(new ItemFragment(), Constants.ITEM_LIST_TAG);
+        adapter.addFragment(new GroupFragment(), Constants.GROUP_LIST_TAG);
         viewPager.setAdapter(adapter);
     }
 
---
+++ eSoochi-android/app/src/main/java/edu/iu/mobiperv/android/esoochi/edu/iu/mobiperv/android/esoochi/util/Constants.java
@@ -0,0 +1,7 @@
+package edu.iu.mobiperv.android.esoochi.edu.iu.mobiperv.android.esoochi.util;
+
+public class Constants {
+
+    public static final String ITEM_LIST_TAG = "ITEMS";
+    public static final String GROUP_LIST_TAG = "GROUPS";
+}
